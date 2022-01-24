efb2268d8df8fe95986be6ce7ea0046e8872b3c0
Ievgenii Maglovanyi <maglovanyi13@gmail.com>
2022-01-24 16:26:58 -0500

    TestCase002-006

--- src/Group_26/TestCase001.java
+++ src/Group_26/TestCase001.java
@@ -4,5 +4,13 @@ public class TestCase001 {
     public static void main(String[] args) {
         System.out.println("let's do it group-26");
         //TestCase002
+
+        //TestCase003
+
+        //TestCase004
+
+        //TestCase005
+
+        //TestCase006
     }
 }
